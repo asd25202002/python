783e11fe694e0e303a7bcf6b1a8d5f51f4b6795a
yui <asd25202002@gmail.com>
2020-08-06 18:26:55 +0800

    更新 README 與 新增資料夾
    目錄.Python 檔案(split)

---
+++ class2/split.py
@@ -0,0 +1,18 @@
+#使用 split
+integers = input("請輸入數字: ")
+integer = integers.split(' ') #以空格隔開
+num1,num2,num3,num4 = integer[0],integer[1],integer[2],integer[3] #使用陣列形式帶進 4 個變數裡面
+print("第1個值: {}\n第2個值: {}\n第3個值: {}\n第4個值: {}\n".format(num1,num2,num3,num4))
+
+
+#使用 split
+integers = input("請輸入數字: ")
+integer = integers.split(' ') #以空格隔開
+num1,num2,num3,num4 = integer
+print("第1個值: {}\n第2個值: {}\n第3個值: {}\n第4個值: {}\n".format(num1,num2,num3,num4))
+
+
+#使用 eval
+integers = eval(input("請輸入數字: "))
+num1,num2,num3,num4 = integers
+print("第1個值: {}\n第2個值: {}\n第3個值: {}\n第4個值: {}\n".format(num1,num2,num3,num4))
